24893f8ab25d96f8b67f2ac7044136f76cc6c2c3
VaishnavChidambar <vaishnav.chidambar@sequoia.com>
2022-01-15 10:39:24 +0530

    Initial Commit

---
+++ Locus's WeatherUITests/Locus_s_WeatherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Locus_s_WeatherUITestsLaunchTests.swift
+//  Locus's WeatherUITests
+//
+//  Created by vaishnav.chidambar on 15/01/22.
+//
+
+import XCTest
+
+class Locus_s_WeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
